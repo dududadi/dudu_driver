d3613468242ea1ef291b368a49c0c7e29eea22d7
planchet <33612803+planchet@users.noreply.github.com>
2017-11-29 16:27:27 +0800

    ycw 11/29 16:27
    更新了order页面

---
+++ pages/order/order.js
@@ -0,0 +1,35 @@
+Page({
+  data: {
+    toView: 'red',
+    scrollTop: 0
+  },
+  tap: function (e) {
+    for (var i = 0; i < order.length; ++i) {
+      if (order[i] === this.data.toView) {
+        this.setData({
+          toView: order[i + 1]
+        })
+        break
+      }
+    }
+  },
+  tapMove: function (e) {
+    this.setData({
+      scrollTop: this.data.scrollTop + 10
+    })
+  },
+  onLoad:function (location) {
+    var wxopid=location.openid;
+    var that=this;
+    wx.request({
+      url: 'https://www.forhyj.cn/miniapp/order/orderList',
+      method:'POST',
+      data: { 'wxopid': wxopid},
+      success:function(res){
+        that.setData({
+          resArr: res.data
+        })
+      }
+    })
+  }
+})
