fd7399b2b47612a6f93e0b0326869d42aa0ce6c2
hyyyyyy <245893478@qq.com>
2017-12-06 12:38:09 +0800

    Hy 12/6 12:37
    修改判断条件

--- pages/wallet/wallet.js
+++ pages/wallet/wallet.js
@@ -61,7 +61,7 @@ Page({
       method: "POST",
       success: function (res) {
         console.log(res);
-        if(res==1)
+        if(res.data==1)
         {
           wx.showModal({
             title: '提示',
